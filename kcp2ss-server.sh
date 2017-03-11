67a05f7969d612890f6913efed5b3f928d3696e7
cool168 <cool168@users.noreply.github.com>
2017-03-11 12:55:44 +0800

    Update kcp2ss-server.sh

--- kcp2ss-server.sh
+++ kcp2ss-server.sh
@@ -22,9 +22,9 @@ echo ${SS_SERVER_PWD='cool168'}
 
 server_linux_amd64 -l $KCP_PORT -t 127.0.0.1:$SS_SERVER_PORT -mode $MODE -mtu $MTU -sndwnd $SNDWND -rcvwnd $RCVWND -crypt $CRYPT -key $KEY 2>&1 &
 
-echo -e "\033[32mStarting kcptun......\033[0m"
+echo -e "Starting kcptun......"
 
-echo -e "\033[32mStarting shadowsocks......\033[0m"
+echo -e "Starting shadowsocks......"
 
 ss-server -s 127.0.0.1 -p $SS_SERVER_PORT -m $SS_SERVER_METHOD -k $SS_SERVER_PWD -t 60 -u
 
