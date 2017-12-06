13eb0ab3148201a7168b6b30886bc1587bc5e631
cool168 <cool168@users.noreply.github.com>
2017-12-06 17:22:56 +0800

    Create kcpraw-client.sh

---
+++ kcpraw-client.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo ${LOCAL_PORT=':12948'}
+
+echo ${KCP_PORT='vps:39900'}
+
+echo ${MODE='-mode fast2 -ds 0 -ps 0 -nocomp'}
+	
+echo ${MTU=1400}
+
+echo ${KEY='cool168'}
+
+sleep 1
+kcpraw_client_linux_amd64 -l $LOCAL_PORT -r $KCP_PORT  -key $KEY -mtu $MTU -mode $MODE
