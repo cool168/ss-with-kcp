ff7fdc46ab206c5b1700e2d1e47fae6c39672bd4
cool168 <cool168@users.noreply.github.com>
2020-11-12 18:03:35 +0800

    Update ss-server.sh

--- ss-server.sh
+++ ss-server.sh
@@ -8,11 +8,13 @@ echo ${SS_SERVER_PORT=8989}
 
 echo ${SS_SERVER_METHOD='aes-256-cfb'}
 
+echo ${SS_TIMEOUT=300}
+
 sleep 1
 
 if [ $ENABLE_CFGFILE = "yes" ]
 then
 	ss-server -u -c /etc/ss-config.json
 else
-	ss-server -s :: -s 0.0.0.0 -p $SS_SERVER_PORT -m $SS_SERVER_METHOD -k $SS_SERVER_PWD -t 60 -u
+	ss-server -s :: -s 0.0.0.0 -p $SS_SERVER_PORT -m $SS_SERVER_METHOD -k $SS_SERVER_PWD -t $SS_TIMEOUT -u
 fi
