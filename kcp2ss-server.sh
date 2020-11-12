c6333ff65e3f7287f5ae05525d96b96184e00e6c
cool168 <cool168@users.noreply.github.com>
2020-11-12 17:58:08 +0800

    Update kcp2ss-server.sh

--- kcp2ss-server.sh
+++ kcp2ss-server.sh
@@ -22,7 +22,7 @@ echo ${SS_SERVER_PWD='cool168'}
 
 echo ${SS_SERVER_PASSTHROUGH='no'}
 
-echo ${SS_SERVER_TIMEOUT=300}
+echo ${SS_TIMEOUT=300}
 
 server_linux_amd64 -l $KCP_PORT -t 127.0.0.1:$SS_SERVER_PORT -mode $MODE -mtu $MTU -sndwnd $SNDWND -rcvwnd $RCVWND -crypt $CRYPT -key $KEY 2>&1 &
 
@@ -32,7 +32,7 @@ echo -e "Starting shadowsocks......"
 
 if [ $SS_SERVER_PASSTHROUGH = "yes" ]
 then
-   ss-server -s :: -s 0.0.0.0 -p $SS_SERVER_PORT -m $SS_SERVER_METHOD -k $SS_SERVER_PWD -t $SS_SERVER_TIMEOUT -u
+   ss-server -s :: -s 0.0.0.0 -p $SS_SERVER_PORT -m $SS_SERVER_METHOD -k $SS_SERVER_PWD -t $SS_TIMEOUT -u
 else
-   ss-server -s 127.0.0.1 -p $SS_SERVER_PORT -m $SS_SERVER_METHOD -k $SS_SERVER_PWD -t $SS_SERVER_TIMEOUT -u
+   ss-server -s 127.0.0.1 -p $SS_SERVER_PORT -m $SS_SERVER_METHOD -k $SS_SERVER_PWD -t $SS_TIMEOUT -u
 fi
